a77f94d6eb06105ffa3b22f6a10552cba36bf469
vraj <145261386+Vrajpatel1104@users.noreply.github.com>
2026-09-06 22:36:50 +0530

    All setup configured

--- src/app.js
+++ src/app.js
@@ -13,4 +13,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(cookieParser());
 
+
+//route imports
+import userRoute from './routes/user.route.js';
+
+
+//route declaration
+app.use("/api/v1/users", userRoute);
+
 export default app;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import dotenv from 'dotenv';
-import { connectDB } from './db/index.js';
 dotenv.config({ path: './env' });
+import { connectDB } from './db/index.js';
+import app from './app.js';
 
 connectDB();
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log("Server is running on port: ", process.env.PORT);
+})
