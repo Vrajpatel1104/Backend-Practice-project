875318ff0dcf9964b2223ee4fab0324839f897eb
vraj <145261386+Vrajpatel1104@users.noreply.github.com>
2026-09-18 10:30:20 +0530

    Export getWatchHistory function and update user routes to include it

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import {
   changePassword,
+  getUser,
+  getUserChannelProfile,
+  getWatchHistory,
   loginUser,
   logout,
   refreshAccessToken,
-  registerUser
+  registerUser,
+  updateAvatar,
+  updateCoverImage,
+  updateUser
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -31,4 +37,11 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logout);
 router.route("/refresh-token").post(refreshAccessToken)
 router.route("/change-password").post(verifyJWT, changePassword)
+router.route("/current-user").get(verifyJWT, getUser)
+router.route("/update-account").patch(verifyJWT, updateUser)
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateCoverImage)
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/watch-history").get(verifyJWT, getWatchHistory)
+
 export default router
